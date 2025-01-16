3f311074f35bc892695e2691c84c0c0838c19e19
Ebadullah Jamal <ebadullahjamal394@gmail.com>
2025-01-16 21:41:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+adjective1 = input("Write an Adjective: ")
+adjective2 = input("Write another Adjective: ")
+verb1 = input("Write a Verb: ")
+verb2 = input("Write another Verb: ")
+famous_actor = input("Write a Famous Actor: ")
+
+madlib = f"I was working on my computer when I saw {famous_actor}. The computer {verb1} and looked {adjective1}, so I quickly {verb2} \nto fix it. After a few minutes, everything seemed {adjective2} again."
+
+print(madlib)
+
